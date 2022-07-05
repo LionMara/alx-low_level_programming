ef9b2991f10d463d54adb5ea3caab1e2b8a1c304
LionMara <lionofmara@gmail.com>
2022-07-05 14:10:22 -0700

    Bettying around is necessary

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,25 +4,24 @@
  * @n: the integer to be checked
  *
  * Return: 1 if positive
- * Return: 0 if zero
- * Return: -1 if less zero
+ * If equal to zero return 0
+ * otherwise return -1 if less zero
  */
 int print_sign(int n)
 {
-  if (n > 0)
-    {
-      _putchar('+');
-      return (1);
-    }
-  else if (n < 0)
-    {
-      _putchar('-');
-      return (-1);
-    }
-  else
-    {
-      _putchar('0');
-      return (0);
-    }
+if (n > 0)
+{
+_putchar('+');
+return (1);
 }
-	
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
