638bd752b48fe5e95f171998b6e4bd87753b389f
LionMara <lionofmara@gmail.com>
2022-07-05 13:36:08 -0700

    debugging betty style.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _islower : checks if char is lower
- * @c: The char
+ * _islower - checks if char is lower
+ * @c: The char to be checked.
  *
- * Return: on succes 1
+ * Return: on success 1
  * Return 0 if char is not lower
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-  {
-    return 1;
-  }
- else
-   {
-     return 0;
-   }
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
