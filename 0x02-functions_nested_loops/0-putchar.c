06e42cea1aa0e25f4a9788c979def0b7aa251c34
LionMara <lionofmara@gmail.com>
2022-07-05 07:48:49 -0700

    Adding my first header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+/*int main(void)
 {
   printf("_putchar\n");
   return (0);
-}
+  }*/
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+int main(void)
+{
+  printf("_putchar\n");
+  return (0);
+}
