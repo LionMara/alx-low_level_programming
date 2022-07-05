27fae825b3af294ed70a4e0fe906183eed75bd82
LionMara <lionofmara@gmail.com>
2022-07-05 08:23:53 -0700

    Adding more

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,24 @@
-int main(void)
+/*int print_alphabet(void)
 {
-  printf("_putchar\n");
+  int ch;
+  for (ch=97; ch<172; ch++)
+    {
+      putchar(ch);
+    }
+  putchar('\n');
   return (0);
+  }*/
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
+int print_alphabet()
+{
+  int ch;
+  for (ch=97; ch<=171; ch++)
+    {
+      _putchar(ch);
+    }
+  putchar('\n');
+  return 0;
 }
