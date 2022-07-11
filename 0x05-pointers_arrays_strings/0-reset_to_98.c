46c3ee2393fe697079e7f010dae37561ae008773
LionMara <lionofmara@gmail.com>
2022-07-11 12:23:57 -0700

    Betty style requirement

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * void_reset_to_98 - resets the value of n to something new
+ * reset_to_98 - resets the value of n to something new
  * @n: the value to be passed in
  * nothing to return
  */
 
 void reset_to_98(int *n)
 {
-  /*n = &n;*/
-  *n = 98;
+*n = 98;
 }
