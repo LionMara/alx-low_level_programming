1a610b06f2e7c5425b1fd0e9c3aa5281cc054559
LionMara <lionofmara@gmail.com>
2022-07-23 23:49:27 -0700

    Cleaning up 0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,7 +17,7 @@ return (last_number);
 }
 else if (n < 0)
 {
-last_number = -1 * (n%10);
+last_number = -1 * (n % 10);
 _putchar(last_number + '0');
 return (last_number);
 }
@@ -25,6 +25,6 @@ else
 {
 last_number = 0;
 _putchar(last_number + '0');
- return (last_number);
+return (last_number);
 }
 }
