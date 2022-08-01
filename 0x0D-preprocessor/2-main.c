8ccd3c2d0aff548329b38d5b572c8140729980de
LionMara <lionofmara@gmail.com>
2022-08-01 17:31:20 +0000

    must be the easiest so far

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print name of file
+ *
+ * Return: something
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
