17668a92826f01c603347f6e5890abe765b1f29b
LionMara <lionofmara@gmail.com>
2022-08-23 12:14:23 +0300

    Free and close those files, son

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len, writecount;
-	char *buf = malloc(sizeof text_content);
+	char *buf = malloc(sizeof(text_content));
+
 	buf = text_content;
 
 	len = 0;
@@ -18,13 +19,16 @@ int create_file(const char *filename, char *text_content)
 		len++;
 
 	if (buf == NULL)
+	{
+		free(buf);
 		return (0);
-	if(filename == NULL)
+	}
+	if (filename == NULL)
 		return (-1);
-	if(text_content == NULL)
+	if (text_content == NULL)
 		open(filename, O_CREAT, O_EXCL);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600 /*S_IRUSR | S_IWUSR*/);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 	{
 		printf("I did not succeed\n");
@@ -36,5 +40,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+
+	free(buf);
+	close(fd);
 	return (1);
 }
