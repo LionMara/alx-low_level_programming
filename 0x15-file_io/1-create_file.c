763697c2ea544b258f39d251cb745ec5aad49e8e
LionMara <lionofmara@gmail.com>
2022-08-23 11:00:14 +0300

    The good stuff

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: content to be passed in
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, writecount;
+	char *buf = malloc(sizeof text_content);
+	buf = text_content;
+
+	len = 0;
+	while (text_content[len] != '\0')
+		len++;
+
+	if (buf == NULL)
+		return (0);
+	if(filename == NULL)
+		return (-1);
+	if(text_content == NULL)
+		open(filename, O_CREAT, O_EXCL);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600 /*S_IRUSR | S_IWUSR*/);
+	if (fd < 0)
+	{
+		printf("I did not succeed\n");
+		return (-1);
+	}
+
+	writecount = write(fd, buf, len);
+	if (writecount < 0)
+	{
+		return (-1);
+	}
+	return (1);
+}
