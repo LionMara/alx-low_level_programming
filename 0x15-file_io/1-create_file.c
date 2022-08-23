ce35c5bec945df05babbd25615ee10d7575875c8
LionMara <lionofmara@gmail.com>
2022-08-23 17:25:05 +0300

    This check 7 is  just eating me up

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,6 @@ int create_file(const char *filename, char *text_content)
 	buf = text_content;
 
 	len = 0;
-	while (text_content[len] != '\0')
-		len++;
 
 	if (buf == NULL)
 	{
@@ -25,13 +23,16 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600 );
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd < 0)
 	{
 		return (-1);
 	}
-	if(text_content)
+	if (text_content != NULL)
 	{
+		while (text_content[len] != '\0')
+		len++;
+
 		writecount = write(fd, buf, len);
 		if (writecount < 0)
 		{
