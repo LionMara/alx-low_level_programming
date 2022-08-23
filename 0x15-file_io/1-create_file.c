673f3047d38ad023f2b91287e76f61787beb490f
LionMara <lionofmara@gmail.com>
2022-08-23 16:03:45 +0300

    Null texts solved

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * create_file - creates a file
  * @filename: name of file
@@ -25,19 +24,18 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		open(filename, O_CREAT, O_EXCL);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600 );
 	if (fd < 0)
 	{
 		return (-1);
 	}
-
-	writecount = write(fd, buf, len);
-	if (writecount < 0)
+	if(text_content)
 	{
-		return (-1);
+		writecount = write(fd, buf, len);
+		if (writecount < 0)
+		{
+			return (-1);
+		}
 	}
 
 	close(fd);
