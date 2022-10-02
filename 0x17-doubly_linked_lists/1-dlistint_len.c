deb1b9c029ce5c46fdd522c666ca710276513d75
LionMara <lionofmara@gmail.com>
2022-10-02 18:13:11 +0300

    Yes yes

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * dlistint_len - prints contents of a dlist
+ * @h: what to be printed
+ *
+ * Return: returns number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	{
+		exit(1);
+	}
+	else
+	{
+		i = 0;
+
+		while (h != NULL)
+		{
+			/*printf("%d\n", h->n);*/
+			h = h->next;
+			i++;
+		}
+	}
+
+	return (i);
+}
