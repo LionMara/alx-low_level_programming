7a888f7d828768674b88929a931552bc8d3ee057
LionMara <lionofmara@gmail.com>
2022-10-02 19:03:56 +0300

    Trial

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
-	if (h == NULL)
-	{
-		return (EXIT_FAILURE);
-	}
-	else
-	{
+	/*
+	 *if (h == NULL)
+	 *{
+	 *	return (EXIT_FAILURE);
+	 *	}
+	 */
+
 		i = 0;
 
 		while (h != NULL)
@@ -24,7 +25,5 @@ size_t print_dlistint(const dlistint_t *h)
 			h = h->next;
 			i++;
 		}
-	}
-
 	return (i);
 }
