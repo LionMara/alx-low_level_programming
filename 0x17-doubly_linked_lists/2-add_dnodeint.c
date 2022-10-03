3c49922b142b9841858680fc537a7bd571d2ed27
LionMara <lionofmara@gmail.com>
2022-10-03 08:43:13 +0300

    Pointers aren't dreadful

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a node to the
+ * beginning of a doubly linked list
+ *
+ * @head: list
+ * @n: what to be added
+ *
+ * Return: address of new element, or
+ * NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *ptr = (dlistint_t*)malloc(sizeof(dlistint_t)), *tmp;
+	tmp = *head;
+	if (ptr == NULL)
+	{
+		exit(1);
+	}
+	ptr->n = n;
+	ptr->prev = NULL;
+	if (tmp == NULL)
+		ptr->next = NULL;
+	else
+	{
+		while (tmp->prev)
+			tmp = tmp->prev;
+		tmp->prev = ptr;
+		ptr->next = tmp;
+	}
+	*head = ptr;
+	return (*head);
+}
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds node at the end
+ * @head: list
+ * @n: what to be added
+ *
+ * Return: adress of new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *ptr = (dlistint_t*)malloc(sizeof(dlistint_t)), *tmp;
+	if (ptr == NULL)
+		exit(1);
+
+	ptr->n = n;
+	ptr->next = NULL;
+	tmp = *head;
+
+	while(tmp && tmp->next)
+		tmp = tmp->next;
+
+	if (tmp)
+	{
+		ptr->prev = tmp;
+		tmp->next = ptr;
+	}
+	else
+	{
+		*head = ptr;
+		ptr->prev = NULL;
+	}
+	return (ptr);
+}
