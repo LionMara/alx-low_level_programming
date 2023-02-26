b4ab3eaf9512dd6ae26badfdece009733f4c0003
LionMara <lionofmara@gmail.com>
2023-02-26 14:53:31 +0300

    Some additions

---
+++ 0x1C-makefiles/algo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void f(int n)
+{
+	int i;
+	int j;
+	for (i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			for (j = 1; j < n; j = j * 2)
+			{
+				printf("[%d] [%d]", i, j);
+			}
+		}
+		else
+		{
+			for (j = 0; j < n; j = j + 2)
+			{
+				printf("[%d] [%d]", i, j);
+			}
+		}
+	}
+}
